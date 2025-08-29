baf5a251050dc00ecd8e0677718c8dec7bb5dac8
vonnue-assin <assin.anna@vonnue.com>
2025-08-29 09:18:51 +0530

    resolved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import './styles.css';
-import LeadingSystems from '../components/LeadingSystems';
-import LeadingSystemsData from '../data/leadingSystems.json';
 
 export const Home = () => {
-  return <LeadingSystems />;
+  return <div></div>;
 };
